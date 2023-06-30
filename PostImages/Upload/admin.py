432775175e56dff4cddd2cdae09ff8f5d05d6c6a
akshitasana740 <92258481+akshitasana740@users.noreply.github.com>
2023-06-30 09:41:30 +0530

    Add files via upload

---
+++ PostImages/Upload/admin.py
@@ -0,0 +1,5 @@
+from django.contrib import admin
+from .models import ImageModel
+@admin.register(ImageModel)
+class ImageAdmin(admin.ModelAdmin):
+ list_display = ['id', 'photo', 'status','posted_on']
